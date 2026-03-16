d350067003624bd6b9eabb2c970ddb033a938187
marlboro-red <chanjun.park@wisetechglobal.com>
2026-03-16 11:28:03 +1100

    E2E split panes tests with store exposure (smoke-03p.19)

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -6,9 +6,10 @@ import { connectorStore } from './stores/connectorStore'
 import { goToLineStore } from './fileviewer/goToLineStore'
 import { toastStore } from './stores/toastStore'
 import { shortcutBindingsStore } from './shortcuts/shortcutMap'
+import { splitPaneStore } from './stores/splitPaneStore'
 
 // Expose stores on window for E2E testing
-;(window as any).__SMOKE_STORES__ = { sessionStore, connectorStore, goToLineStore, toastStore, shortcutBindingsStore }
+;(window as any).__SMOKE_STORES__ = { sessionStore, connectorStore, goToLineStore, toastStore, shortcutBindingsStore, splitPaneStore }
 
 const root = createRoot(document.getElementById('root')!)
 root.render(<App />)
